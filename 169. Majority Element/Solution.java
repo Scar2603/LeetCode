19e639eb677e8296cbd685c0e57a2d5270d5725b
Sanskar Kannurkar <107759303+Sanskar-2603@users.noreply.github.com>
2023-11-09 03:19:01 +0530

    Solution.java

--- 169. Majority Element/Solution.java
+++ 169. Majority Element/Solution.java
@@ -1,1 +1,10 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // Step 1: Sort the input array in ascending order.
+        Arrays.sort(nums);
 
+        // Step 2: The majority element will always be at the middle index of the sorted array,
+        // since it appears more than n/2 times. The majority element is guaranteed to exist.
+        return nums[nums.length/2];
+    }
+}
