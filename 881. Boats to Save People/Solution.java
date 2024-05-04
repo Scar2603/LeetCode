2a1933cdb65dded6a11f6b67c6f1e8dd6240426a
Sanskar Kannurkar <107759303+Scar2603@users.noreply.github.com>
2024-05-04 18:16:07 +0530

    Solution.java

---
+++ 881. Boats to Save People/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int num = 0;
+        
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            num++;
+        }
+        
+        return num;
+    }
+}
